b0ec1bce5ccfe509e9ae2582418fc295bc7cfc14
madinabokoum8 <mbokoum@albany.edu>
2020-02-04 11:19:00 -0500

    added file

---
+++ argument_lab.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "There were $# arguments"
+echo "The arguements were: $*"
+
+echo "There were $# arguments"
+echo "The arguments were: $*"
+
+echo 'Last name, First name:'
+echo "The script is called $0"
+
+
+sum=$(( $1 + $2 ))
+product=$(( $1 * $2 ))
+echo "The sum of $1 and $2 is $sum"
+echo "The product of $1 and $2 is $product"
+
